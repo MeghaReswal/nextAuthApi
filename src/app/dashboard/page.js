2f1363393cde819584338656d0a8dfd2b1581e8c
megha <megha@meelance.com>
2024-08-22 19:32:24 +0530

    updated

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,34 @@
+"use client";
+
 import "./style.css";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export default function Dashboard() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [router]);
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <>
       <section>
         <div className="container">
           <div className="login">
-            <h>Welcome to dashboard</h>
+            <h1>Welcome to the Dashboard</h1>
           </div>
         </div>
       </section>
